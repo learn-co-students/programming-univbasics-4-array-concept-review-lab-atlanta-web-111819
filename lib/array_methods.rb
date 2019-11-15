270d4d0a1f45852ff598036adcd0be9ca26f136b
Jessica Michael <pantherjmm@yahoo.com>
2019-11-15 04:19:38 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,23 @@
+'''ruby
+
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  friends = ["Sam", "Bob", "Tom"]
+end
+
+counter = 0
+friends = ["Sam", "Bob", "Tom"]
+
+while friends[counter] do
+  puts friends[counter]
+  counter += 1
 end
 
 def find_max_value(array)
-  # Add your solution here
+  [1,2,4,5,7,56,7]
 end
+=> 56
 
 def find_min_value(array)
-  # Add your solution here
+  [10,4343,1324,244595,19,12,434,2]
 end
+=> 2
